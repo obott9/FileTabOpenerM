c483f9770c6515a3482903ecd31d5121b42cc24e
obote <obott@e-mail.jp>
2026-02-24 03:08:54 +0900

    Initial Commit

---
+++ FileTabOpenerM/FileTabOpenerMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileTabOpenerMApp.swift
+//  FileTabOpenerM
+//
+//  Created by 尾保手　秀樹 on 2026/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FileTabOpenerMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FileTabOpenerMTests/FileTabOpenerMTests.swift
@@ -0,0 +1,17 @@
+//
+//  FileTabOpenerMTests.swift
+//  FileTabOpenerMTests
+//
+//  Created by 尾保手　秀樹 on 2026/02/24.
+//
+
+import Testing
+@testable import FileTabOpenerM
+
+struct FileTabOpenerMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
